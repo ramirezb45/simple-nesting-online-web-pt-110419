4bdb8b0ed840697326c6d846e2dd59d911548486
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-11-14 02:14:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
 end
-
+programmer_hash[:dennis_ritchies_language]
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
