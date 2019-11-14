07a871ee0177ec28622eabda6a1769a82da7fbbc
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-11-14 02:06:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
- hopper == [:programmer_hash :grace_hopper{}]
+ hopper == [:programmer_hash :grace_hopper:known_for:languages]
 
 puts programmer_hash
 
